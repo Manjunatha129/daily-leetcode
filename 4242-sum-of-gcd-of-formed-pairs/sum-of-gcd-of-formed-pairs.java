4991f0d7a6b0934d55590cfadd29fec56b22258c
K . Manjunatha <manjunathak129@gmail.com>
2026-07-16 07:46:50 +0530

    Time: 53 ms (95.50%) | Memory: 108.3 MB (47.00%) - LeetSync

--- 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.java
+++ 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.java
@@ -1,35 +1,40 @@
 class Solution {
     public long gcdSum(int[] nums) {
-        int maxElement  = 0;
-        int NewArray[] = new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] > maxElement){
+        int maxElement = 0;
+        int[] NewArray = new int[nums.length];
+        
+        // 1. Construct prefixGcd array
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > maxElement) {
                 maxElement = nums[i];
-
             }
-            NewArray[i] = gcd(nums[i] , maxElement);
-
+            NewArray[i] = gcd(nums[i], maxElement);
         }
+        
+        // 2. Sort the array
         Arrays.sort(NewArray);
-    
-
+        
+        // 3. Form pairs using two pointers and sum their GCDs
         int left = 0;
-        int right = NewArray.length-1;
-        long sum = 0;
-        while(left < right){
-            sum += gcd(NewArray[left],NewArray[right]);
+        int right = NewArray.length - 1;
+        long sum = 0; // Changed to long to prevent overflow
+        
+        while (left < right) {
+            sum += gcd(NewArray[left], NewArray[right]);
             left++;
             right--;
-
         }
+        
         return sum;
     }
-       private int gcd(int a,int b){
-            while(b != 0){
-                int reminder = a % b;
-                a = b;
-                b = reminder;
-            }
-            return a;
+    
+    // Correctly defined helper method outside of gcdSum
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int remainder = a % b;
+            a = b;
+            b = remainder;
         }
+        return a;
+    }
 }
